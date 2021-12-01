95c5f544acc5d661d3b2a9f87e700221ee70aba1
Joe Spadafora <joe.spadafora@onepeloton.com>
2021-12-01 11:13:27 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AdventOfCode2021",
+    platforms: [.macOS(.v10_14)],
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-parsing.git",
+            from: "0.3.0"
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "AdventOfCode2021",
+            dependencies: [
+                .product(name: "Parsing", package: "swift-parsing")
+            ]),
+        .testTarget(
+            name: "AdventOfCode2021Tests",
+            dependencies: ["AdventOfCode2021"]),
+    ]
+)
